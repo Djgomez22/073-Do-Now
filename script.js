a222aa8c6b5fd961e441661f350c6341db2a85df
Hello-World <144818870+Djgomez22@users.noreply.github.com>
2023-12-19 10:04:27 -0500

    Created using Popcode: https://popcode.org

---
+++ script.js
@@ -0,0 +1,11 @@
+var currentYear = 2023;
+
+var birthYear = 2005;
+
+var name = "Duane Gomez";
+
+var mood = "Sleepy";
+
+console.log(currentYear - birthYear);
+
+console.log("My name is " +name+ " And Im " + mood );
